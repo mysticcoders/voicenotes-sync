ad3a8b8ce3fe965a91dfb3ed078f0dfb5f51e331
Henri Jamet <henri.jamet@epita.fr>
2024-08-04 13:13:52 +0200

    feat: :rocket: Add subnotes

--- main.ts
+++ main.ts
@@ -127,6 +127,141 @@ export default class VoiceNotesPlugin extends Plugin {
 		)).filter(filename => filename !== undefined) as string[];
 	}
 
+	async processNote(recording: any, voiceNotesDir: string, isSubnote: boolean = false): Promise<void> {
+		if (!recording.title) {
+			new Notice(`Unable to grab voice recording with id: ${recording.id}`);
+			return;
+		}
+
+		if (this.syncedRecordingIds.includes(recording.recording_id)) {
+			return;
+		}
+
+		const title = this.sanitizedTitle(recording.title, recording.created_at);
+		const recordingPath = normalizePath(`${voiceNotesDir}/${title}.md`);
+
+		let note = '---\n';
+		note += `recording_id: ${recording.recording_id}\n`;
+		note += `duration: ${Math.floor(recording.duration / 60000) > 0
+			? `${Math.floor(recording.duration / 60000)}m`
+			: ''
+			}${Math.floor((recording.duration % 60000) / 1000).toString().padStart(2, '0')}s\n`;
+		note += `created_at: ${recording.created_at}\n`;
+		note += `updated_at: ${recording.updated_at}\n`;
+
+		if (recording.tags && recording.tags.length > 0) {
+			note += `tags: ${recording.tags.map((tag: { name: string }) => tag.name).join(",")}\n`;
+		}
+		note += '---\n';
+
+		if (this.settings.downloadAudio) {
+			const audioPath = normalizePath(`${voiceNotesDir}/audio`);
+
+			if (!await this.app.vault.adapter.exists(audioPath)) {
+				await this.app.vault.createFolder(audioPath);
+			}
+			const outputLocationPath = normalizePath(`${audioPath}/${recording.recording_id}.mp3`);
+
+			if (!await this.app.vault.adapter.exists(outputLocationPath)) {
+				const signedUrl = await this.vnApi.getSignedUrl(recording.recording_id);
+				await this.vnApi.downloadFile(this.fs, signedUrl.url, outputLocationPath);
+			}
+
+			note += `![[${recording.recording_id}.mp3]]\n\n`;
+		}
+
+		const summary = recording.creations.find((creation: { type: string }) => creation.type === 'summary');
+		const points = recording.creations.find((creation: { type: string }) => creation.type === 'points');
+		if (summary) {
+			note += '# Summary\n\n' + (summary.content.data as string) + '\n\n';
+		}
+		if (points) {
+			note += '# Key Points\n\n' + (points.content.data as string) + '\n\n';
+		}
+		if (recording.transcript) {
+			const tidyTranscript = recording.creations.find((creation: { type: string }) => creation.type === 'tidy');
+			if (this.settings.replaceTranscriptWithTidy && tidyTranscript) {
+				note += '# Tidy Transcript\n\n' + tidyTranscript.content.data + '\n\n';
+			} else {
+				note += '# Transcript\n\n' + recording.transcript + '\n\n';
+			}
+		}
+
+		for (const creation of recording.creations) {
+			if (creation.type === 'summary' || creation.type === 'points' || (creation.type === 'tidy' && this.settings.replaceTranscriptWithTidy)) {
+				continue;
+			}
+
+			note += `## ${capitalizeFirstLetter(creation.type)}\n\n`;
+
+			if (creation.type === 'email') {
+				const creationData = creation.content.data as VoiceNoteEmail;
+				note += `**Subject:** ${creationData.subject}\n\n`;
+				note += `${creationData.body}\n`;
+			} else if (creation.type === 'todo') {
+				const creationData = creation.content.data as string[];
+
+				if (Array.isArray(creationData)) {
+					note += creationData.map(data => `- [ ] ${data}${this.settings.todoTag ? ' #' + this.settings.todoTag : ''}`).join('\n');
+				}
+			} else {
+				const creationData = creation.content.data as string;
+				note += creationData;
+				note += '\n';
+			}
+		}
+
+		if (recording.attachments && recording.attachments.length > 0) {
+			note += '\n## Attachments\n';
+			note += (await Promise.all(recording.attachments.map(async (data: { type: number, description?: string, url?: string }) => {
+				if (data.type === 1) {
+					return `- ${data.description}`;
+				} else if (data.type === 2) {
+					const attachmentsPath = normalizePath(`${voiceNotesDir}/attachments`);
+
+					if (!await this.app.vault.adapter.exists(attachmentsPath)) {
+						await this.app.vault.createFolder(attachmentsPath);
+					}
+
+					const filename = getFilenameFromUrl(data.url);
+					const attachmentPath = normalizePath(`${attachmentsPath}/${filename}`);
+					await this.vnApi.downloadFile(this.fs, data.url, attachmentPath);
+					return `- ![[${filename}]]`;
+				}
+			}))).join('\n');
+			note += '\n';
+		}
+
+		if (!isSubnote) {
+			if (recording.related_notes && recording.related_notes.length > 0) {
+				note += '\n## Related Notes\n\n';
+				note += recording.related_notes.map((relatedNote: { title: string; created_at: string }) => `- [[${this.sanitizedTitle(relatedNote.title, relatedNote.created_at)}]]`).join('\n');
+			}
+
+			if (recording.subnotes && recording.subnotes.length > 0) {
+				note += '\n### Subnotes\n\n';
+				for (const subnote of recording.subnotes) {
+					await this.processNote(subnote, voiceNotesDir, true);
+					note += `- [[${this.sanitizedTitle(subnote.title, subnote.created_at)}]]\n`;
+				}
+			}
+		}
+
+		if (await this.app.vault.adapter.exists(recordingPath)) {
+			await this.app.vault.modify(this.app.vault.getFileByPath(recordingPath), note);
+		} else {
+			await this.app.vault.create(recordingPath, note);
+		}
+
+		this.syncedRecordingIds.push(recording.recording_id);
+
+		// DESTRUCTIVE action which will delete all synced recordings from server if turned on
+		// We ask twice to make sure user is doubly sure
+		if (this.settings.deleteSynced && this.settings.reallyDeleteSynced) {
+			await this.vnApi.deleteRecording(recording.recording_id)
+		}
+	}
+
 	async sync(fullSync: boolean = false) {
 		console.debug(`Sync running full? ${fullSync}`)
 
@@ -153,138 +288,10 @@ export default class VoiceNotesPlugin extends Plugin {
 			} while (nextPage)
 		}
 
-		// console.dir(recordings)
 		if (recordings) {
 			new Notice(`Syncing latest Voicenotes`)
 			for (const recording of recordings.data) {
-				if (!recording.title) {
-					new Notice(`Unable to grab voice recording with id: ${recording.id}`)
-					continue
-				}
-
-				// If we've already synced it locally let's not do it again
-				if (this.syncedRecordingIds.includes(recording.recording_id)) {
-					continue;
-				}
-
-				const title = this.sanitizedTitle(recording.title, recording.created_at)
-				const recordingPath = normalizePath(`${voiceNotesDir}/${title}.md`)
-
-				let note = '---\n'
-				note += `recording_id: ${recording.recording_id}\n`
-				note += `duration: ${Math.floor(recording.duration / 60000) > 0
-					? `${Math.floor(recording.duration / 60000)}m`
-					: ''
-					}${Math.floor((recording.duration % 60000) / 1000).toString().padStart(2, '0')}s\n`
-				note += `created_at: ${recording.created_at}\n`
-				note += `updated_at: ${recording.updated_at}\n`
-
-				if (recording.tags.length > 0) {
-					note += `tags: ${recording.tags.map(tag => tag.name).join(",")}\n`
-				}
-				note += '---\n'
-
-				if (this.settings.downloadAudio) {
-					const audioPath = normalizePath(`${voiceNotesDir}/audio`)
-
-					if (!await this.app.vault.adapter.exists(audioPath)) {
-						await this.app.vault.createFolder(audioPath)
-					}
-					const outputLocationPath = normalizePath(`${audioPath}/${recording.recording_id}.mp3`);
-
-					if (!await this.app.vault.adapter.exists(outputLocationPath)) {
-						// Get unique audio download URL and download file to disk
-						const signedUrl = await this.vnApi.getSignedUrl(recording.recording_id)
-						await this.vnApi.downloadFile(this.fs, signedUrl.url, outputLocationPath);
-					}
-
-					note += `![[${recording.recording_id}.mp3]]\n\n`
-				}
-
-				// Add the summary, key points and transcript to the note
-				const summary = recording.creations.find(creation => creation.type === 'summary');
-				const points = recording.creations.find(creation => creation.type === 'points');
-				if (summary) {
-					note += '# Summary\n\n' + (summary.content.data as string) + '\n\n';
-				}
-				if (points) {
-					note += '# Key Points\n\n' + (points.content.data as string) + '\n\n';
-				}
-				if (recording.transcript) {
-					const tidyTranscript = recording.creations.find(creation => creation.type === 'tidy');
-					if (this.settings.replaceTranscriptWithTidy && tidyTranscript) {
-						note += '# Tidy Transcript\n\n' + tidyTranscript.content.data + '\n\n';
-					} else {
-						note += '# Transcript\n\n' + recording.transcript + '\n\n';
-					}
-				}
-
-				for (const creation of recording.creations) {
-
-					// Skip summary, points and tidy if we're replacing transcript with tidy
-					if (creation.type === 'summary' || creation.type === 'points' || (creation.type === 'tidy' && this.settings.replaceTranscriptWithTidy)) {
-						continue;
-					}
-
-					note += `## ${capitalizeFirstLetter(creation.type)}\n\n`
-
-					if (creation.type === 'email') {
-						const creationData = creation.content.data as VoiceNoteEmail
-						note += `**Subject:** ${creationData.subject}\n\n`
-						note += `${creationData.body}\n`
-					} else if (creation.type === 'todo') {
-						const creationData = creation.content.data as string[]
-
-						if (Array.isArray(creationData)) {
-							note += creationData.map(data => `- [ ] ${data}${this.settings.todoTag ? ' #' + this.settings.todoTag : ''}`).join('\n')
-						}
-					} else {
-						const creationData = creation.content.data as string
-						note += creationData
-						note += '\n'
-					}
-				}
-
-				if (recording.attachments.length > 0) {
-					note += '\n## Attachments\n'
-					note += (await Promise.all(recording.attachments.map(async data => {
-						if (data.type === 1) {
-							return `- ${data.description}`
-						} else if (data.type === 2) {
-							const attachmentsPath = normalizePath(`${voiceNotesDir}/attachments`)
-
-							if (!await this.app.vault.adapter.exists(attachmentsPath)) {
-								await this.app.vault.createFolder(attachmentsPath)
-							}
-
-							const filename = getFilenameFromUrl(data.url)
-							const attachmentPath = normalizePath(`${attachmentsPath}/${filename}`)
-							await this.vnApi.downloadFile(this.fs, data.url, attachmentPath);
-							return `- ![[${filename}]]`
-						}
-					}))).join('\n')
-					note += '\n'
-				}
-
-				if (recording.related_notes.length > 0) {
-					note += '\n## Related Notes\n\n'
-					note += recording.related_notes.map(relatedNote => `- [[${this.sanitizedTitle(relatedNote.title, relatedNote.created_at)}]]`).join('\n')
-				}
-				console.debug(`Writing ${recording.recording_id} to ${recordingPath}`)
-
-				if (await this.app.vault.adapter.exists(recordingPath)) {
-					await this.app.vault.modify(this.app.vault.getFileByPath(recordingPath), note)
-				} else {
-					await this.app.vault.create(recordingPath, note)
-				}
-
-				this.syncedRecordingIds.push(recording.recording_id)
-
-				// DESTRUCTIVE action which will delete all synced recordings from server if turned on
-				// We ask twice to make sure user is doubly sure
-				if (this.settings.deleteSynced && this.settings.reallyDeleteSynced) {
-					await this.vnApi.deleteRecording(recording.recording_id)
-				}
+				await this.processNote(recording, voiceNotesDir);
 			}
 		}
 
